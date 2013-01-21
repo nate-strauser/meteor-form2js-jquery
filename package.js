da3dc3db769d01cce4dbf8a69ca00076ae402420
Nathan Strauser <nathan.strauser@gmail.com>
2013-01-21 15:44:09 -0500

    add jquery

--- package.js
+++ package.js
@@ -3,6 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
+    api.use('jquery', 'client');
     api.add_files([
         "form2js/src/form2js.js",
         "form2js/src/jquery.toObject.js",
