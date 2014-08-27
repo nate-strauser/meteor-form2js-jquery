e0b59791a52f8cdca3578887249d504353afa5bd
Nathan Strauser <nathan.strauser@gmail.com>
2014-08-27 11:47:56 -0400

    meteor 0.9.0

--- package.js
+++ package.js
@@ -1,8 +1,12 @@
 Package.describe({
-    summary: "Javascript library for collecting form data with jquery wrapper"
+	name:"natestrauser:form2js-jquery",
+    summary: "Javascript library for collecting form data with jquery wrapper",
+  version: "0.1.3",
+  git: "https://github.com/nate-strauser/meteor-form2js-jquery.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
     api.use('jquery', 'client');
     api.add_files([
         "lib/form2js/src/form2js.js",
